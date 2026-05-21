8ef1fc93620c94fad44f498a41fdb6231f83c218
Muhammad Abid Dewantoro <151120880+DeadEye27@users.noreply.github.com>
2026-05-21 14:53:09 +0700

    feat: rock-paper-scissors logic done

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ using Math.random() that return a random number between 0 - 1
 jadi nanti math random akan di bagi 3, kurang dari sama dengan 0.4, 
 lebih dari 0.4 dan kurang dari 0.6, dan Lebih dari sama dengan 0.6
 
+buat variabel randomNumber
 computer akan mendapatkan rock jika angka yang didapatkan di variabel 
 randomNumber kurang dari sama dengan 0.4 
 
@@ -20,6 +21,27 @@ maka computer akan mendapatkan scissors sebagai pilihan
 pilihan computer akan di masukan ke dalam variable computerChoice 
 yang berupa string dan akan di outputkan
 
+*/
+
+const getComputerChoice = () => {
+    const randomNumber = Math.random();
+    let computerChoice;
+
+    if (randomNumber <= 0.4) {
+        computerChoice = "rock";
+    } else if (randomNumber >= 0.4 && randomNumber <= 0.6) {
+        computerChoice = "paper";
+    } else if (randomNumber >= 0.6) {
+        computerChoice = "scissors"
+    }
+
+    console.log(randomNumber);
+    // console.log(computerChoice);
+    return computerChoice;
+};
+
+
+/*
     FUNGSI : getHumanChoice
 buat fungsi getHumanChoice()
 Mengambil input user menggunakan methode prompt
@@ -30,6 +52,23 @@ Buat function bernama getHumanChoice yang berfungsi untuk mengoutputkan input us
 user memasukan input pilihan gunting batu kertas
 input dari user akan di jadi kan lower case dengan fungsi toLowerCase
 
+*/
+
+const getHumanChoice = () => {
+    // console.log(humanChoice);
+    isValid = false
+    do {
+        let rawHumanChoice = prompt("Masukan Pilihan Anda");
+        let humanChoice = rawHumanChoice.toLowerCase()
+        if (humanChoice == "paper" || humanChoice == "rock" || humanChoice == "scissors") {
+            isValid = true;
+            return humanChoice;
+        }
+    } while (isValid);
+};
+
+
+/*
     Mencatat Hasil Permainan / Score
 
 Membuat Variable humanScore dan computerScore
@@ -38,7 +77,11 @@ Jika user menang, humanScore ditambah 1
 jika komputer menang, computerScore ditambah 1
 jika hasil draw (pilihan nya sama), humanScore dan computerScore tidak bertambah atau tetap
 
-    FUNGSI : playRound(humanChoice, computerChoice)
+*/
+
+
+/*
+    FUNGSI : playRound(humanChoice, computerChoice)l
 
 tulis logika permainan batu gunting kertas
 input yang diberikan adalah humanChoice dan computerChoice
@@ -49,10 +92,60 @@ jika humanChoice menang maka humanScore tambah 1 poin
 jika computerChoice menang maka computerScore tambah 1 poin
 jika seri maka ngga ada yang bertambah baik humanScore maunpun computerScore
 
+*/
+
+let humanScore = 0;
+let computerScore = 0;
+const playRound = (humanChoice, computerChoice) => {
+    let humanSelection = humanChoice;
+    let computerSelection = computerChoice;
+
+    if ((humanSelection == "rock" && computerSelection == "paper") || (humanSelection == "paper" && computerSelection == "scissors") || (humanSelection == "scissors" && computerSelection == "rock")) {
+        console.log(`pilihan user: ${humanSelection}`);
+        console.log(`pilihan komputer: ${computerSelection}`);
+        console.log("Komputer Menang");
+        computerScore = computerScore + 1;
+    } else if ((humanSelection == "paper" && computerSelection == "rock") || (humanSelection == "scissors" && computerSelection == "paper") || (humanSelection == "rock" && computerSelection == "scissors")) {
+        console.log(`pilihan user: ${humanSelection}`);
+        console.log(`pilihan komputer: ${computerSelection}`);
+        console.log("User Menang");
+        humanScore = humanScore+1;
+    } else {
+        console.log(`pilihan user: ${humanSelection}`);
+        console.log(`pilihan komputer: ${computerSelection}`);
+        console.log("Seri");
+    }
+
+    console.log(`score User : ${humanScore}`);
+    console.log(`score komputer : ${computerScore}`);
+    
+    
+}
+
+const playGame = () => {
+    for (let i = 1; i < 6; i++) {
+        isValid = false;
+        let finalHumanChoice = "";
+        do {
+            let rawHumanChoice = prompt("Masukan Pilihan Anda");
+            let humanChoice = rawHumanChoice.toLowerCase()
+            if (humanChoice == "paper" || humanChoice == "rock" || humanChoice == "scissors") {
+                isValid = true;
+                finalHumanChoice = humanChoice
+                // return humanChoice;
+            }
+            alert("perhatikan input anda");
+        } while (!isValid);
+        playRound(finalHumanChoice, getComputerChoice());
+        console.log(`Permainan ke : ${i}`);
+    }
+}
+
+/*
     FUNGSI : playGame()
 
 Masukan untuk deklarasi fungsi playRound() dan varibel yang berfungsi menyimpan score ke fungsi playGame() 
 panggil playRound() sebanyak 5 kali agar permainan bisa berjalan selama 5 kali
 
 */
-
+playGame();
